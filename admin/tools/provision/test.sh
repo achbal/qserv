36e9951477e1b9f8fe4c72a100d649b584dadffd
Oualid Achbal <aaoualid@gmail.com>
2016-05-24 12:48:45 +0200

    Improve TEST script

--- admin/tools/provision/test.sh
+++ admin/tools/provision/test.sh
@@ -1,3 +1,8 @@
+set -x
+set -e
+
 nova list | grep "jammes-qserv" | cut -d'|' -f 2| xargs nova delete
 nova image-delete centos-7-qserv
-python create-image.py && sleep 10 && python provision-qserv.py
+python create-image.py
+sleep 60
+python provision-qserv.py
